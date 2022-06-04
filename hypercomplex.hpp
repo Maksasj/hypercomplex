27ffcfa7657b8f5def78a2f166f4d81a68a2dace
Maksasj <radioboos@gmail.com>
2022-06-04 16:22:59 +0300

    basic octonions

--- hypercomplex.hpp
+++ hypercomplex.hpp
@@ -4,12 +4,18 @@
 #include <string>
 #include <iostream>
 #include <cmath>
+#include <utility>
 
 template<typename T> T conj(T x) {
       T xstar = x*(-1.0);
       xstar.a *= -1;
       return xstar;
 }
+
+template<class T1, class T2> struct Cpair {
+      T1 a1;
+      T2 a2;
+};
 
 struct imaginary {
       float a;
@@ -42,7 +48,6 @@ struct complex {
             };
       }
 
-
       friend std::ostream& operator<<(std::ostream& os, complex other) {
             os << other.a << (other.i.a > 0 ? '+' : '-') << abs(other.i.a) << "i";
             return os;
@@ -54,25 +59,20 @@ struct quaternion: complex {
       imaginary k;
 
       quaternion(float _a, float _i, float _j, float _k): complex(_a, _i) { j.a = _j; k.a = _k; };
-      quaternion(float _a, imaginary _i, imaginary _j, imaginary _k): complex(_a, _i) { j = _j; k = _k; };
-      quaternion(complex z, float _j, float _k): complex(z.a, z.i.a) { j.a = _j; k.a = _k; };
-      quaternion(complex z, imaginary _j, imaginary _k): complex(z.a, z.i.a) { j = _j; k = _k; };
+
+      Cpair<complex, complex> cut(quaternion q) {
+            return {{q.a, q.i.a}, {q.j.a, q.k.a}};
+      }
 
       quaternion operator-(quaternion const& other) { return {a - other.a, i - other.i, j - other.j, k - other.k}; }
       quaternion operator+(quaternion const& other) { return {a + other.a, i + other.i, j + other.j, k + other.k}; }
-      
+      quaternion operator*(float const& x) { return {(a*x), (i.a*x), (j.a*x), (k.a)*x}; }
       quaternion operator*(quaternion const& other) { 
-            quaternion q1 = {a, i.a, j.a, k.a};
-            quaternion q2 = {other.a, other.i.a, other.j.a, other.k.a};
+            auto p1 = cut({a, i.a, j.a, k.a});
+            auto p2 = cut(other);
 
-            complex Ca = {q1.a, q1.i.a};
-            complex Cb = {q1.j.a, q1.k.a};
-
-            complex Cc = {q2.a, q2.i.a};
-            complex Cd = {q2.j.a, q2.k.a};
-            
-            complex qp1 = {Ca*Cc - conj(Cd)*Cb};
-            complex qp2 = {Cd*Ca + Cb*conj(Cc)};
+            complex qp1 = {p1.a1*p2.a1 - conj(p2.a2)*p1.a2};
+            complex qp2 = {p2.a2*p1.a1 + p1.a2*conj(p2.a1)};
 
             quaternion q = {qp1.a, qp1.i.a, qp2.a, qp2.i.a};
             return q;
@@ -90,9 +90,54 @@ struct quaternion: complex {
       */
 
       friend std::ostream& operator<<(std::ostream& os, quaternion other) {
-            os    << other.a << (other.i.a > 0 ? '+' : '-') << fabs(other.i.a) << "i" 
+            os    << other.a 
+                  << (other.i.a > 0 ? '+' : '-') << fabs(other.i.a) << "i" 
                   << (other.j.a > 0 ? '+' : '-') << fabs(other.j.a) << "j" 
                   << (other.k.a > 0 ? '+' : '-') << fabs(other.k.a) << "k";
+            return os;
+      }
+};
+
+struct octonion: quaternion {
+      imaginary e5;
+      imaginary e6;
+      imaginary e7;
+      imaginary e8;
+
+      octonion(   float _a, float _i, float _j, float _k,
+                  float _e5, float _e6, float _e7, float _e8
+      ): quaternion(_a, _i, _j, _k) { 
+            e5.a = _e5;
+            e6.a = _e6;
+            e7.a = _e7;
+            e8.a = _e8;
+      };
+
+      Cpair<quaternion, quaternion> cut(octonion o) {
+            return {{o.a, o.i.a, o.j.a, o.k.a}, {o.e5.a, o.e6.a, o.e7.a, o.e8.a}};
+      }
+
+      octonion operator*(octonion const& other) { 
+            auto p1 = cut({a, i.a, j.a, k.a, e5.a, e6.a, e7.a, e8.a});
+            auto p2 = cut(other);
+
+            quaternion qp1 = {p1.a1*p2.a1 - conj(p2.a2)*p1.a2};
+            quaternion qp2 = {p2.a2*p1.a1 + p1.a2*conj(p2.a1)};
+
+            octonion q = {qp1.a, qp1.i.a, qp1.j.a, qp1.k.a, qp2.a, qp2.i.a, qp2.j.a, qp2.k.a};
+            return q;
+      }
+
+      friend std::ostream& operator<<(std::ostream& os, octonion other) {
+            os    << other.a 
+                  << (other.i.a > 0 ? '+' : '-') << fabs(other.i.a) << "i" 
+                  << (other.j.a > 0 ? '+' : '-') << fabs(other.j.a) << "j" 
+                  << (other.k.a > 0 ? '+' : '-') << fabs(other.k.a) << "k"
+
+                  << (other.e5.a > 0 ? '+' : '-') << fabs(other.e5.a) << "e5"
+                  << (other.e6.a > 0 ? '+' : '-') << fabs(other.e6.a) << "e6"
+                  << (other.e7.a > 0 ? '+' : '-') << fabs(other.e7.a) << "e7"
+                  << (other.e8.a > 0 ? '+' : '-') << fabs(other.e8.a) << "e8";
             return os;
       }
 };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <vector>
 #include "hypercomplex.hpp"
+
+template<typename T> void printH(T a1, T a2) {
+      std::cout << "(" << a1 << ")*(" << a2 << ") = " << a1*a2 << "\n";
+}
 
 int main() {
       complex q1 = {3, 1};
@@ -8,12 +13,11 @@ int main() {
       quaternion q4 = {1 , 6, -4, 1};
       quaternion q5 = {-2 , 5, 7, -3};
 
-      quaternion q6 = {q1, -6.0, 5.0};
-      std::cout << q6 << "\n";
-
-      std::cout << "(" << q1 << ")*(" << q2 << ") = " << q1*q2 << "\n";
-      std::cout << "(" << q4 << ")*(" << q5 << ") = " << q4*q5 << "\n";
-      std::cout << "(" << q4 << ")*(" << q5 << ") = " << q4+q5 << "\n";
-      std::cout << "(" << q1 << ")*(" << q1 << ") = " << q1*q1 << "\n";
+      octonion o1 = {7, 3, 5, -6, 8, 2, 9, 1};
+      octonion o2 = {1, -6, 4, 3, 8, 8, 9, 9};
+      
+      printH(q1, q2);
+      printH(q4, q5);
+      printH(o1, o2);
       return 0; 
 }

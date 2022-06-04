60a4cf30d007850e4ee33a0dba21f554cb6bdbf9
Maksasj <radioboos@gmail.com>
2022-06-04 15:17:41 +0300

    another quaternion multiplication

--- hypercomplex.hpp
+++ hypercomplex.hpp
@@ -5,6 +5,12 @@
 #include <iostream>
 #include <cmath>
 
+template<typename T> T conj(T x) {
+      T xstar = x*(-1.0);
+      xstar.a *= -1;
+      return xstar;
+}
+
 struct imaginary {
       float a;
       
@@ -24,9 +30,18 @@ struct complex {
       imaginary i;
 
       complex(float _a, float _i) { a = _a; i.a = _i; };
+      complex(float _a, imaginary _i) { a = _a; i = _i; };
+
       complex operator+(complex const& other) { return {a + other.a, i + other.i}; }
       complex operator-(complex const& other) { return {a - other.a, i - other.i}; }
-      complex operator*(complex const& other) { return {(a*other.a - i.a*other.i.a), (a*other.i.a + i.a*other.a)}; }
+      complex operator*(float const& x) { return {(a*x), (i.a*x)}; }
+      complex operator*(complex const& other) {
+            return {
+                  (a*other.a - i.a*other.i.a),
+                  (a*other.i.a + i.a*other.a)
+            };
+      }
+
 
       friend std::ostream& operator<<(std::ostream& os, complex other) {
             os << other.a << (other.i.a > 0 ? '+' : '-') << abs(other.i.a) << "i";
@@ -37,10 +52,33 @@ struct complex {
 struct quaternion: complex {
       imaginary j;
       imaginary k;
-      quaternion(float _a, float _i, float _j, float _k): complex(_a, _i) { j.a = _j; k.a = _k; };
 
-      quaternion operator+(quaternion const& other) { return {a + other.a, i + other.i, j + other.j, k + other.k}; }
+      quaternion(float _a, float _i, float _j, float _k): complex(_a, _i) { j.a = _j; k.a = _k; };
+      quaternion(float _a, imaginary _i, imaginary _j, imaginary _k): complex(_a, _i) { j = _j; k = _k; };
+      quaternion(complex z, float _j, float _k): complex(z.a, z.i.a) { j.a = _j; k.a = _k; };
+      quaternion(complex z, imaginary _j, imaginary _k): complex(z.a, z.i.a) { j = _j; k = _k; };
+
       quaternion operator-(quaternion const& other) { return {a - other.a, i - other.i, j - other.j, k - other.k}; }
+      quaternion operator+(quaternion const& other) { return {a + other.a, i + other.i, j + other.j, k + other.k}; }
+      
+      quaternion operator*(quaternion const& other) { 
+            quaternion q1 = {a, i.a, j.a, k.a};
+            quaternion q2 = {other.a, other.i.a, other.j.a, other.k.a};
+
+            complex Ca = {q1.a, q1.i.a};
+            complex Cb = {q1.j.a, q1.k.a};
+
+            complex Cc = {q2.a, q2.i.a};
+            complex Cd = {q2.j.a, q2.k.a};
+            
+            complex qp1 = {Ca*Cc - conj(Cd)*Cb};
+            complex qp2 = {Cd*Ca + Cb*conj(Cc)};
+
+            quaternion q = {qp1.a, qp1.i.a, qp2.a, qp2.i.a};
+            return q;
+      }
+
+      /*
       quaternion operator*(quaternion const& other) { 
             return {
                   (a*other.a - i.a*other.i.a - j.a*other.j.a - k.a*other.k.a),
@@ -49,6 +87,7 @@ struct quaternion: complex {
                   (a*other.k.a + i.a*other.j.a - j.a*other.i.a + k.a*other.a)
             };
       }
+      */
 
       friend std::ostream& operator<<(std::ostream& os, quaternion other) {
             os    << other.a << (other.i.a > 0 ? '+' : '-') << fabs(other.i.a) << "i" 
--- main.cpp
+++ main.cpp
@@ -5,10 +5,15 @@ int main() {
       complex q1 = {3, 1};
       complex q2 = {5, -3};
       
-      quaternion q4 = {-1.69 , 6.2, -8.1, 6.42};
-      quaternion q5 = {56.0 , -1.3, 69.2, 0.46};
+      quaternion q4 = {1 , 6, -4, 1};
+      quaternion q5 = {-2 , 5, 7, -3};
+
+      quaternion q6 = {q1, -6.0, 5.0};
+      std::cout << q6 << "\n";
 
       std::cout << "(" << q1 << ")*(" << q2 << ") = " << q1*q2 << "\n";
       std::cout << "(" << q4 << ")*(" << q5 << ") = " << q4*q5 << "\n";
+      std::cout << "(" << q4 << ")*(" << q5 << ") = " << q4+q5 << "\n";
+      std::cout << "(" << q1 << ")*(" << q1 << ") = " << q1*q1 << "\n";
       return 0; 
 }
